9ec872fb3c72995cd506bb7dd29fda787a68680d
PhuocTran <phuoc.tran@hubcba.com>
2018-05-28 21:48:13 +0700

    Update about

--- src/pages/home/About.js
+++ src/pages/home/About.js
@@ -11,12 +11,13 @@ const About = () => (
         <a href="mailto:tranhuuphuoc88@gmail.com">tranhuuphuoc88@gmail.com</a>
         {`, Skype: tranhuuphuoc88, Birthday: 12/05/1988`}
       </div>
+      <div className='text-primary mb-5'>Apply for the position: <strong>ReactJS/Android technical lead</strong></div>
       <p className="mb-5" style={{width: 1000, textAlign: 'justify'}}>
         I was born and grown in a countryside in Ha Tinh province. With a passion for computer from very soon.
         I become a Senior software engineer in Android/ReactJs/Java backend with over 7 years of experience.
         After graduated I spend a few months work in FPT Software in Da Nang city at the position Android developer, 3 years in Gameloft just focus on Video Game,
         2 years in Foody and build up a product on Android for more than 38 million users,
-        1 year work Freelancer (In that duration have a few months work for VPBank) then back to FPT again and work at customer side is CBA (A software development HUB belong Commonwealth Bank) from 03/2016 until now.
+        1 year work Freelancer (In that duration have a few months work for VPBank) then back to FPT again with the role is Solution Architecture and work ODC at customer side is CBA (A software development HUB belong Commonwealth Bank) from 03/2016 until now.
         During that time I've got lots of hands-on experiences at the positions like Android developer, ReactJs web developer, Backend developer, Scrum Master, DevOpts and Automation QA.
         Beside that I usually organize outdoor activities, team building, charity events for team and company. So that everyone usually called me is "party leader" ^^.
       </p>
