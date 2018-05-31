82b5bc69e662740d7ecebe1ddf52979a0ad4ac56
PhuocTran <phuoc.tran@hubcba.com>
2018-05-31 11:31:38 +0700

    Add react helmet for SEO

---
+++ src/client.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Router from 'react-router'
+
+window.React = React
+
+let routes = require('./routes')
+
+Router.run(routes, Router.HistoryLocation, function (Root, state) {
+  React.render(<Root/>, document.getElementById('app'))
+
+  console.log('Rendered app on client')
+})
--- src/pages/home/App.js
+++ src/pages/home/App.js
@@ -8,11 +8,17 @@ import Education from './Education'
 import Skills from './Skills'
 import Interests from './Interests'
 import Certifications from './Certifications'
+import Helmet from 'react-helmet/es/Helmet'
 
 class App extends Component {
   render () {
     return (
       <div className='wrapper'>
+        <Helmet
+          title="About"
+          meta={[
+            {property: 'og:title', content: 'About'},
+          ]} />
         <Menu/>
         <div className="container-fluid p-0">
           <About/>
